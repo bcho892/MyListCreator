13c539bb6fcbe19adf66e5eabdbfed859969d47f
bcho892 <bcho892@aucklanduni.ac.nz>
2022-06-24 00:58:51 +1200

    added new components

---
+++ src/components/pages/searchandadd/InfoCard.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+
+export default function InfoCard() {
+  return (
+
+    <Paper elevation={0} sx={{width:"100%",minWidth:"20rem", minHeight:"20rem",height:{ md:"20rem", lg: "100%" }, backgroundColor: "#E0E0E0"}} />
+    
+  );
+}
