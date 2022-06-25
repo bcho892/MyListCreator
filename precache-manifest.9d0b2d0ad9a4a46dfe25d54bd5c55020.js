2e146b20b610c3b8b6ed994f2add95633b18177f
bcho892 <bcho892@aucklanduni.ac.nz>
2022-06-25 16:04:26 +1200

    Updates

---
+++ precache-manifest.9d0b2d0ad9a4a46dfe25d54bd5c55020.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f366015b0beeb1c36e4f",
+    "url": "/MyListCreator/static/css/main.cb8557d7.chunk.css"
+  },
+  {
+    "revision": "f366015b0beeb1c36e4f",
+    "url": "/MyListCreator/static/js/main.f366015b.chunk.js"
+  },
+  {
+    "revision": "86ee184744c529ac9b16",
+    "url": "/MyListCreator/static/js/1.86ee1847.chunk.js"
+  },
+  {
+    "revision": "c4c7e6a8165761ad1572",
+    "url": "/MyListCreator/static/js/2.c4c7e6a8.chunk.js"
+  },
+  {
+    "revision": "f17696004533e02691bf",
+    "url": "/MyListCreator/static/js/runtime~main.f1769600.js"
+  },
+  {
+    "revision": "6059d0978d0a46f5bc7eb7486672bda7",
+    "url": "/MyListCreator/static/media/github.6059d097.svg"
+  },
+  {
+    "revision": "1b7791f8038111f28ecfefd7f7b722fd",
+    "url": "/MyListCreator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.073b7ad03788b8e325c5404a7eb3cd67.js"
+  "/MyListCreator/precache-manifest.9d0b2d0ad9a4a46dfe25d54bd5c55020.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/MyListCreator/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
